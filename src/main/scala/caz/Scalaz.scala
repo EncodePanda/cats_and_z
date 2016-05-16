b3020e3e591cbdda227db55ab56e5f459c8265c7
Marek Kliś <marek.klis@gmail.com>
2016-05-16 12:23:10 +0200

    Apply for scalaz

--- src/main/scala/caz/Scalaz.scala
+++ src/main/scala/caz/Scalaz.scala
@@ -44,6 +44,12 @@ object ScalazApp extends App {
   println("Functor")
   println("****************")
 
+  val len: String => Int = str => str.length()
+  val maybeName: Option[String] = "name".some
+
+  // use function designed for wrapped type
+  println(maybeName.map(len))
+
   def sendMessage[F[_]](users: F[String])(implicit functor: Functor[F]) = {
     // functor.map(users)(s => s + "!")
     users.map(s => s + "!")
@@ -60,5 +66,27 @@ object ScalazApp extends App {
   val add: (Int, Int) => Int = (a: Int, b: Int) => a + b
 
   println(oab.apply(add))
+
+  println("****************")
+  println("Apply")
+  println("****************")
+
+  // from functor
+  println(Functor[Option].map(maybeName)(len))
+  println(maybeName.map(len))
+
+  val maybeLen: Option[String => Int] = len.some
+  println(Apply[Option].ap(maybeName)(maybeLen))
+
+  println("maybeName <*> maybeLen: " + (maybeName <*> maybeLen))
+
+  // apply two argument function
+  val isLong: (String, Int) => Boolean = (str, bound) => len(str) > bound
+  val maybeBoundry: Option[Int] = 10.some
+
+  val temp: String => (Int => Boolean) = isLong.curried
+  val temp2: Option[Int => Boolean] = maybeName.map(isLong.curried)
+
+  println(maybeBoundry <*> maybeName.map(isLong.curried))
 
 }
